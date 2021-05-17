676b4694078130d218e2167dd8a4fc5c2204d4de
Arundhati012 <78207187+Arundhati012@users.noreply.github.com>
2021-05-17 12:44:19 +0530

    Add files via upload

---
+++ hero.js
@@ -0,0 +1,19 @@
+class Hero{
+    constructor(x,y,r){
+        var options={
+            density:1,
+            frictionAir:1
+
+        };
+        this.x=x;
+        this.y=y;
+        this.r=r;
+      this.image=loadImage("Superhero-01.png");
+      this.body=Bodies.circle(this.x,this.y(this.r)/2,options);
+        World.add(world,this.body);
+    }
+    
+};
+function mousePressed(){
+    Matter.Body.setPosition(hero.body,{x:mouseX,y:mouseY});
+}
